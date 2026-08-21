7d95339f84080bc57c28f5644577248d18f2b065
ova889 <ovagg889@gmail.com>
2026-08-20 18:47:40 -0600

    config(app): apuntar a api de produccion en Render (mercurio-9haf)

--- app/lib/config.dart
+++ app/lib/config.dart
@@ -4,7 +4,7 @@
 /// - Para tu celular contra tu máquina usa la IP local:  http://192.168.x.x:8847
 /// - Producción (Render):  https://el-bunker.onrender.com
 class AppConfig {
-  static const String apiBase = 'http://10.0.2.2:8847';
+  static const String apiBase = 'https://mercurio-9haf.onrender.com';
   static const String joinCode = 'BUNKER-6';
 
   static String get wsUrl =>
